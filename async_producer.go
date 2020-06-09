945143ad5891ba04cae565c80aec9ef585fb1e4e
Fae Charlton <fae.charlton@elastic.co>
2020-06-09 16:46:04 -0400

    Only call returnError once per incoming message

--- async_producer.go
+++ async_producer.go
@@ -519,6 +519,7 @@ func (pp *partitionProducer) dispatch() {
 		}
 	}()
 
+	leaderRetries := 0
 	for msg := range pp.input {
 		if pp.brokerProducer != nil && pp.brokerProducer.abandoned != nil {
 			select {
@@ -561,14 +562,16 @@ func (pp *partitionProducer) dispatch() {
 		// if we made it this far then the current msg contains real data, and can be sent to the next goroutine
 		// without breaking any of our ordering guarantees
 
-		for retries := 0; pp.brokerProducer == nil; retries++ {
-			// On success, updateLeader sets brokerProducer, satisfying the loop condition, otherwise we report
-			// the error and wait for the backoff.
+		if pp.brokerProducer == nil {
 			if err := pp.updateLeader(); err != nil {
+				// Report the error on this message and wait for the backoff, then move on to the next one.
 				pp.parent.returnError(msg, err)
-				pp.backoff(retries)
+				pp.backoff(leaderRetries)
+				leaderRetries++
 				continue
 			}
+			// On success, reset the retry count to 0.
+			leaderRetries = 0
 			Logger.Printf("producer/leader/%s/%d selected broker %d\n", pp.topic, pp.partition, pp.leader.ID())
 		}
 
